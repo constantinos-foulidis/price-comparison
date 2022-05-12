53b6f1d8f13bf14a25fd3f6838ab1f7c9daf4188
cfoulidis <cfoulidis@arx.gr>
2022-05-12 19:16:43 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  // { path: 'auth',
+  //   // canActivate: [UnauthorizedGuard],
+  //   loadChildren: () => AuthModule
+  // },
+   {
+    path: '',
+    // canActivate: [AuthorizedGuard],
+    loadChildren: () => import('./application/application.module').then(m => m.ApplicationModule)
+   }
+  // // {
+  // //   path: 'company',
+  // //   //canActivate: [AuthorizedGuard,
+  // //   SelectedCompanyGuard],
+  // //   loadChildren: () => import('../company/company.module').then(m => m.CompanyModule)
+  // // }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
